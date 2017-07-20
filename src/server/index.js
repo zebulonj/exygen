8f02aebea83ffb8630f24c95ad66eca731c94a84
Zebulon Young <zebulonj@gmail.com>
2017-07-20 08:17:49 -0700

    Move webpack-dev-middleware from exygen middleware to server.

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import proxy from 'http-proxy-middleware';
+import webpack from './webpack';
 
 import middleware from './middleware';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
 const defaultOptions = {
+  webpackConfig: {},
   port: 8080
 };
 
@@ -11,7 +16,15 @@ export function server( options = {} ) {
 
   console.log( "[exygen-server] Starting..." );
 
+  const { webpackConfig } = options;
   const server = express();
+
+  if ( NODE_ENV === 'development' ) {
+    const rules = ( ( webpackConfig.devServer || {} ).proxy || {} );
+    Object.keys( rules ).map( path => server.use( path, proxy( rules[path] ) ) );
+
+    server.use( webpack( webpackConfig ) );
+  }
 
   server.use( middleware( options ) );
 
--- src/server/middleware.js
+++ src/server/middleware.js
@@ -1,12 +1,9 @@
 import express, { Router } from 'express';
-import proxy from 'http-proxy-middleware';
 import thunk from 'redux-thunk';
 
-import webpack from './webpack';
 import reactor from './reactor';
 
 const defaultOptions = {
-  webpackConfig: {},
   initialState: {},
   middleware: [
     thunk
@@ -19,16 +16,9 @@ const defaultOptions = {
 export default function exygenMiddleware( options = {} ) {
   options = Object.assign({}, defaultOptions, options );
 
-  const { Wrapper, routes, reducer, middleware, assets, initialState, webpackConfig, scripts } = options;
+  const { Wrapper, routes, reducer, middleware, assets, initialState, scripts } = options;
 
   const router = Router();
-
-  if ( process.env.NODE_ENV !== "production" ) {
-    const rules = ( ( webpackConfig.devServer || {} ).proxy || {} );
-    Object.keys( rules ).map( path => router.use( path, proxy( rules[path] ) ) );
-
-    router.use( webpack( webpackConfig ) );
-  }
 
   if ( assets ) {
     router.use( express.static( assets ) );
